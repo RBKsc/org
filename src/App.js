92daae38500417f66e035b4bdf5cd5cfca0d8cbc
RBKsc <bksophie@gmail.com>
2024-06-25 22:06:09 -0500

    Crear componente equipos

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import Header from './componentes/header/header';
 import Formulario from './componentes/formulario/Formulario';
 import MiOrg from './componentes/miOrg';
+import Equipo from './componentes/equipo';
 
 
 function App() {
-  const [mostrarFormulario, actualizarMostrar]=useState(true)
+  const [mostrarFormulario, actualizarMostrar]=useState(false)
   const cambiarMostrar=()=>{
     actualizarMostrar(!mostrarFormulario)
   }
+
+  //Lista equipos
+  const Equipos=[
+    {
+      titulo:"Programación",
+      colorPrimario: "#57C278",
+      colorSEcundario:"#D9F7E9"
+    },
+        "Front End",
+        "Data Science",
+        "Devops",
+        "UX y Diseño",
+        "Móvil",
+        "Innovación y Gestión"
+  ]
 
   return (
     <div className="App">
@@ -20,6 +36,13 @@ function App() {
       {mostrarFormulario && <Formulario/> }
       
       <MiOrg cambiarMostrar={cambiarMostrar}/>
+      <Equipo equipo="Programación"/>
+      <Equipo equipo="Front End"/>
+      <Equipo equipo="Data Science"/>
+      <Equipo equipo="Devops"/>
+      <Equipo equipo="UX y Diseño"/>
+      <Equipo equipo="Móvil"/>
+      <Equipo equipo="Innovación y Gestión"/>
       
     </div>
   );
